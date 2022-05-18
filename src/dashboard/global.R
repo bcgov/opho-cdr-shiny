0523d6c0d48e736b50e1662d7cc5901a13e0c450
shyan0903 <shyan@alum.vassar.edu>
2022-05-18 10:49:51 -0700

    Add comments and reformat global.R

--- src/dashboard/global.R
+++ src/dashboard/global.R
@@ -1,22 +1,24 @@
 # This file creates and wrangles the global data frames that are 
 # going to be used in app.R
 
-# Create 3 dataframes 
+# Create 3 empty dataframes that correspond to three rate types:
+#   Incidence Rate, Active Healthcare Contact (HSC) Prevalence, and Lifetime Prevalence
 inc_rate_df <- data.frame()
 hsc_prev_df <- data.frame()
 life_prev_df <- data.frame()
 
-#Read in and concat data
-for (dir in list.dirs("data")[-1]){
-  for (file in list.files(dir)){
-    new_df <- read_csv(paste0(dir,"/",file),
-                       show_col_types = FALSE)|>
-      drop_na(CRUDE_RATE_PER_1000);
-    if (dir == "data/IncidenceRate"){
-      inc_rate_df <- rbind(inc_rate_df,new_df)
-      hsc_prev_df <- rbind(hsc_prev_df,new_df)
-    }else if (dir == "data/LifePrevalence"){
-      life_prev_df <- rbind(life_prev_df,new_df)
+# Read csv files and concatenate rows with the same rate type
+for (dir in list.dirs("data")[-1]) {
+  for (file in list.files(dir)) {
+    new_df <- read_csv(paste0(dir, "/", file),
+                       show_col_types = FALSE) |>
+      drop_na(CRUDE_RATE_PER_1000)
+    
+    if (dir == "data/IncidenceRate") {
+      inc_rate_df <- rbind(inc_rate_df, new_df)
+      hsc_prev_df <- rbind(hsc_prev_df, new_df)
+    } else if (dir == "data/LifePrevalence") {
+      life_prev_df <- rbind(life_prev_df, new_df)
     }
   }
 }
@@ -40,19 +42,19 @@ life_prev_df <- life_prev_df |>
   select(-FISC_YR_LABEL)|>
   data.table::setcolorder(c("YEAR"))
 
-#read in chsa shapefiles
+# Read the shapefiles for the Community Health Service Areas (CHSA) level
 chsa_spdf <- readOGR(
-  dsn = paste0(getwd(),"/geo_data/chsa_2018"),
-  layer ="CHSA_2018",
+  dsn = paste0(getwd(), "/geo_data/chsa_2018"),
+  layer = "CHSA_2018",
   verbose = FALSE
 ) |>
-  spTransform( CRS("+proj=longlat +datum=WGS84 +no_defs"))
+  spTransform(CRS("+proj=longlat +datum=WGS84 +no_defs"))
 
-#read in ha shapefiles
+# Read the shapefiles for the Health Authorities (HA) level
 ha_spdf <- readOGR(
-  dsn = paste0(getwd(),"/geo_data/ha_2018"),
-  layer ="HA_2018",
+  dsn = paste0(getwd(), "/geo_data/ha_2018"),
+  layer = "HA_2018",
   verbose = FALSE
 ) |>
-  spTransform( CRS("+proj=longlat +datum=WGS84 +no_defs"))
+  spTransform(CRS("+proj=longlat +datum=WGS84 +no_defs"))
 
