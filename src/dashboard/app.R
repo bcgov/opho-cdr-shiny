436c0a53dbdc8a186950d7e5b3d3883a049583cc
Jessie Wong <jessie14@student.ubc.ca>
2022-05-30 21:45:22 -0700

    Add rate type info

--- src/dashboard/app.R
+++ src/dashboard/app.R
@@ -21,6 +21,7 @@ library(rgeos)
 # Define and load all the global variables, including the data frames and shape files
 ################################
 source('global.R', local = T) 
+source('info.R', local = T)
 options(spinner.color="#003366")
 
 ################################
@@ -41,62 +42,21 @@ ui <- fluidPage(
       ################################
       navbarMenu("Information",
         tabPanel("About", 
-                 h2("Welcome to the BC Chronic Disease Dashboard"),
+                 p(HTML("<h1><u>Welcome to the BC Chronic Disease Dashboard!</u></h1>")),
                  h5("Developed by Jessie Wong, Jennifer Hoang, Mahmoodur Rahman, and Irene Yan"),
-                 helpText(HTML("<br/><br/>
-                               This dashboard facilitates the exploration and visualization of spatial and temporal
-                               trends of 25 different chronic diseases across the Province of British Columbia. The data 
-                               is sourced from the Chronic Disease Registry (BCCDR).
-                               <br/><br/>
-                               The three tabs in the dashboard and their respective features are described below.<br/>
-                               <ul>
-                                 <li><b>By Disease</b></li>
-                                 This tab allows for the trend comparisons of one disease over several Health Authorities(HA)  
-                                 or Community Health Service Areas (CHSA). In this tab the user should select a rate type, 
-                                 a disease, geography type, year, and gender. The user can also optionally specify 
-                                 multiple HAs or CHSAs. <br/><br/>
-                                 <li><b>By Region</b></li>
-                                 This tab allows for the trend comparisons of several diseases in one particular HA or CHSA.
-                                 In this tab the user should select a rate type, disease(s) of interest, geography type,
-                                 year, and gender. <br/><br/>
-                                 <li><b>Data</b></li>
-                                 This tab retrieves all data specified by the user. In this tab the user should select a rate type,
-                                 disease(s), health region(s), year range, and gender. There is also an option for the user to
-                                 download the selected data.<br/><br/>
-                               </ul>"))),
-           tabPanel("Rate Types",
-                    helpText(HTML(
-                               "The definitions of the rate types are provided below.
-                               <ul>
-                                 <li><b>Incidence Rate</b> : The rate at which new cases of disease occur in a 
-                                 specified population during a specified time period. 
-                                 It is calculated as the number of new cases in the population at-risk in a 
-                                 specified period of time divided by the person-time at risk or the number of persons at risk 
-                                 (i.e., mid-year population in a reporting year minus previous year's prevalent cases) in the same period. 
-                                 <br/><br/>
-                                 <p style='margin-left: 40px'>Incidence rate  = (number of newly identified cases in a reporting year) / 
-                                 (mid-year population at risk in the reporting year) * 10<sup>n</sup></p></li>
-                                 <li><b>Lifetime Prevalence</b>: proportion of individuals who have had the condition for at least part of 
-                                 their lives at any time during their life course. In the BCCDR, this refers to the proportion of residents 
-                                 who were diagnosed/identified as a case at least once and were still alive and residing in the province during 
-                                 a reporting time period (fiscal year). Once the case definition criteria are met in a year, cases are then
-                                 carried forward to count as a case every year thereafter until the person's death, their migration out of BC,
-                                 or the absence of follow-up. 
-                                 <br/><br/>
-                                 <p style='margin-left: 40px'> Lifetime prevalence = (number of residents ever identified with a disease 
-                                 in a reporting year) / (mid-year population in the reporting year) * 10<sup>n</sup></p></li>
-                                 <li><b>Active Healthcare Contact (HSC) Prevalence</b>: For relapsing-remitting diseases, the BCCDR measures 
-                                 active healthcare contact prevalence. Cases are counted if they previously met case definition criteria 
-                                 for a disease, continued to live and receive healthcare services for the disease again in BC during a
-                                 later reporting period. That is, cases are counted for a reporting period if the patient seek healthcare 
-                                 services for relapsing-remitting conditions again after the fiscal year when they were ascertained as a case. 
-                                 <br/><br/>
-                                 <p style='margin-left: 40px'> Active healthcare contact prevalence = (number of patients receiving
-                                 healthcare services for a disease in a reporting year) /(mid-year population in the reporting year) * 10<sup>n</sup>
-                                 </p></li>
-                               </ul>
-                          note about fiscal year  "))),
-        tabPanel("Diseases")),
+                 h6("UBC Master of Data Science Capstone Project"),
+                 p(HTML(about)),
+                 hr(),
+                 helpText(HTML("For internal use only. Do not distribute.<br/>
+                               For questions about this dashboard, please contact hlth.cdrwg@gov.bc.ca"))
+                 ),
+        tabPanel("Rate Types",
+                 p(HTML("<u><h2>Rate Types</h2></u>")),
+                  p(HTML(rates))
+                 ),
+        tabPanel("Diseases",
+                 p(HTML(""
+                 )))),
       
       ################################
       # "By Disease" Tab UI Side Logic
@@ -561,15 +521,6 @@ server <- function(input, output,session) {
     
     dummy_spdf <- data.table::copy(spdf_d())
     
-    # dummy_spdf@data <- spdf_d()@data|>
-    #   mutate(CRUDE_RATE_PER_1000=dummyData$CRUDE_RATE_PER_1000[match(
-    #             if(input$health_bound_d == "Health Authorities") ha_spdf$HA_Name else chsa_spdf$CHSA_Name,
-    #             dummyData$HEALTH_BOUND_NAME)],
-    #          STD_RATE_PER_1000= dummyData$STD_RATE_PER_1000[match(
-    #             if(input$health_bound_d == "Health Authorities") ha_spdf$HA_Name else chsa_spdf$CHSA_Name,
-    #             dummyData$HEALTH_BOUND_NAME)]
-    #          )
-    
     if(input$health_bound_d == "Health Authorities"){
       dummy_spdf@data <- spdf_d()@data|>
           left_join(dummyData,by=c("HA_Name"="HEALTH_BOUND_NAME"))
@@ -577,8 +528,6 @@ server <- function(input, output,session) {
       dummy_spdf@data <- spdf_d()@data|>
         left_join(dummyData,by=c("CHSA_Name"="HEALTH_BOUND_NAME"))
     }
-    
-    
     
     legend_inc <- round_any(unname(quantile(dummyData[[rateInput_d()]],0.8))/5,0.1)
     mybins <- append(seq(round_any(min(dummyData[[rateInput_d()]]),0.05, f = floor),by=legend_inc,length.out=5),Inf)
@@ -626,14 +575,6 @@ server <- function(input, output,session) {
     error$upper <- paste0(sub("\\_.*", "", rateInput_d()),"_UCL_95")
 
     current_map_spdf <- data.table::copy(spdf_d())
-
-    # current_map_spdf@data <- spdf_d()@data|>
-    #   mutate(CRUDE_RATE_PER_1000= year_filtered_map_df$CRUDE_RATE_PER_1000[match(
-    #             spdf_d()@data[[if(input$health_bound_d == "Health Authorities") "HA_Name" else "CHSA_Name"]],
-    #              year_filtered_map_df$HEALTH_BOUND_NAME)],
-    #          STD_RATE_PER_1000= year_filtered_map_df$STD_RATE_PER_1000[match(
-    #              if(input$health_bound_d == "Health Authorities") ha_spdf$HA_Name else chsa_spdf$CHSA_Name,
-    #              year_filtered_map_df$HEALTH_BOUND_NAME)])
     
     if(input$health_bound_d == "Health Authorities"){
       current_map_spdf@data <- spdf_d()@data|>
--- src/dashboard/info.R
+++ src/dashboard/info.R
@@ -3,53 +3,89 @@ about<-"<br/><br/>
   This dashboard facilitates the exploration and visualization of spatial and temporal
   trends of 25 different chronic diseases across the Province of British Columbia. The data 
   is sourced from the BC Chronic Disease Registry (BCCDR). Data is available for three different rate types, 
+  each as a crude rate or age-standardized rate, and is available at the Health Authority (HA) level or the
+  Community Health Services Area (CHSA) level. The data is aggregated per geography, year, and gender. 
+  Detailed information and definitions of the data can be found in the corresponding information pages.
   
   <br/><br/>
-  The dashboard contains 3 tabs, each of which are described below .<br/>
+  The dashboard contains 3 tabs, each of which are described as follows: <br/>
   <ul>
    <li><b>By Disease</b></li>
-   This tab allows for the trend comparisons of one disease over several Health Authorities(HA)  
-   or Community Health Service Areas (CHSA). In this tab the user should select a rate type, 
-   a disease, geography type, year, and gender. The user can also optionally specify 
-   multiple HAs or CHSAs. <br/><br/>
+   This tab allows for the comparisons of one disease over several HAs or CHSAs.
+   In this tab the user should select a disease, a rate type, geography type, gender, and year. 
+   The user can specify multiple HAs or CHSAs. After data has been selected, the following
+   visualizations will be displayed:
+   <ul>
+    <li> A choropleth map showing the distribution of the chosen rate type across the Province of British Columbia in a given year </li>
+    <li> A bar graph showing the chosen rate type for all selected health regions, with upper and lower bounds of the rate </li>
+    <li> A line chart showing trend of chosen rate type for all selected health regions from 2001 to 2020. </li>
+   </ul>
+   All visualizations have interactive elements, where detailed information will appear upon mouse hover. 
+   An animation of changes over year is also available. 
+   <br/><br/>
    <li><b>By Region</b></li>
-   This tab allows for the trend comparisons of several diseases in one particular HA or CHSA.
-   In this tab the user should select a rate type, disease(s) of interest, geography type,
-   year, and gender. <br/><br/>
+   This tab allows for the comparisons of several diseases in one particular HA or CHSA.
+   In this tab the user should select a rate type, disease(s) of interest, geography type, health region,
+   year, and gender. After data has been selected, the following
+   visualizations will be displayed:
+   <ul>
+    <li> A bar graph showing the chosen rate type for all selected diseases </li>
+    <li> A line chart showing trend of chosen rate type for all selected diseases from 2001 to 2020. </li>
+    <li> A map showing location of selected health region </li>
+   </ul>
    <li><b>Data</b></li>
-   This tab retrieves all data specified by the user. In this tab the user should select a rate type,
-   disease(s), health region(s), year range, and gender. There is also an option for the user to
-   download the selected data.<br/><br/>
-  </ul>"
+   This tab retrieves all data specified by the user. In this tab the user should select a rate type, 
+   disease(s), geography type, health region(s), year range, and gender. A data table showing the selected
+   data wil be displayed, and there is an option for the user to download the selected data.<br/><br/>
+  </ul>
+  <br/>
+  <b>Note:</b> The year selector is based on Ministry of Health fiscal years. For example, the year 2001 represents data 
+  from April 1, 2001 to March 31, 2002. 
+"
 
 
-rates<-"The definitions of the rate types are provided below.
-                               <ul>
-                                 <li><b>Incidence Rate</b> : The rate at which new cases of disease occur in a 
-                                 specified population during a specified time period. 
-                                 It is calculated as the number of new cases in the population at-risk in a 
-                                 specified period of time divided by the person-time at risk or the number of persons at risk 
-                                 (i.e., mid-year population in a reporting year minus previous year's prevalent cases) in the same period. 
-                                 <br/><br/>
-                                 <p style='margin-left: 40px'>Incidence rate  = (number of newly identified cases in a reporting year) / 
-                                 (mid-year population at risk in the reporting year) * 10<sup>n</sup></p></li>
-                                 <li><b>Lifetime Prevalence</b>: proportion of individuals who have had the condition for at least part of 
-                                 their lives at any time during their life course. In the BCCDR, this refers to the proportion of residents 
-                                 who were diagnosed/identified as a case at least once and were still alive and residing in the province during 
-                                 a reporting time period (fiscal year). Once the case definition criteria are met in a year, cases are then
-                                 carried forward to count as a case every year thereafter until the person's death, their migration out of BC,
-                                 or the absence of follow-up. 
-                                 <br/><br/>
-                                 <p style='margin-left: 40px'> Lifetime prevalence = (number of residents ever identified with a disease 
-                                 in a reporting year) / (mid-year population in the reporting year) * 10<sup>n</sup></p></li>
-                                 <li><b>Active Healthcare Contact (HSC) Prevalence</b>: For relapsing-remitting diseases, the BCCDR measures 
-                                 active healthcare contact prevalence. Cases are counted if they previously met case definition criteria 
-                                 for a disease, continued to live and receive healthcare services for the disease again in BC during a
-                                 later reporting period. That is, cases are counted for a reporting period if the patient seek healthcare 
-                                 services for relapsing-remitting conditions again after the fiscal year when they were ascertained as a case. 
-                                 <br/><br/>
-                                 <p style='margin-left: 40px'> Active healthcare contact prevalence = (number of patients receiving
-                                 healthcare services for a disease in a reporting year) /(mid-year population in the reporting year) * 10<sup>n</sup>
-                                 </p></li>
-                               </ul>
-                            "
+rates<-"Three different rate types are available. The definitions of each are as follows:
+       <ul>
+         <li><b>Incidence Rate</b> : The rate at which new cases of disease occur in a 
+         specified population during a specified time period. 
+         It is calculated as the number of new cases in the population at-risk in a 
+         specified period of time divided by the person-time at risk or the number of persons at risk 
+         (i.e., mid-year population in a reporting year minus previous year's prevalent cases) in the same period. 
+         <br/><br/>
+         <p style='margin-left: 40px'>Incidence rate  = (number of newly identified cases in a reporting year) / 
+         (mid-year population at risk in the reporting year) * 10<sup>n</sup></p></li>
+         <li><b>Lifetime Prevalence</b>: proportion of individuals who have had the condition for at least part of 
+         their lives at any time during their life course. In the BCCDR, this refers to the proportion of residents 
+         who were diagnosed/identified as a case at least once and were still alive and residing in the province during 
+         a reporting time period (fiscal year). Once the case definition criteria are met in a year, cases are then
+         carried forward to count as a case every year thereafter until the person's death, their migration out of BC,
+         or the absence of follow-up. 
+         <br/><br/>
+         <p style='margin-left: 40px'> Lifetime prevalence = (number of residents ever identified with a disease 
+         in a reporting year) / (mid-year population in the reporting year) * 10<sup>n</sup></p></li>
+         <li><b>Active Healthcare Contact (HSC) Prevalence</b>: For relapsing-remitting diseases, the BCCDR measures 
+         active healthcare contact prevalence. Cases are counted if they previously met case definition criteria 
+         for a disease, continued to live and receive healthcare services for the disease again in BC during a
+         later reporting period. That is, cases are counted for a reporting period if the patient seek healthcare 
+         services for relapsing-remitting conditions again after the fiscal year when they were ascertained as a case. 
+         <br/><br/>
+         <p style='margin-left: 40px'> Active healthcare contact prevalence = (number of patients receiving
+         healthcare services for a disease in a reporting year) /(mid-year population in the reporting year) * 10<sup>n</sup>
+         </p></li>
+       </ul>
+       <br/><br/>
+      Each of the rates is available as either a crude rate or age-standardized rate. The defintions of these two types are as follows:
+       <ul>
+        <li><b>Age-Standardized</b>:
+        To account for differences in the age structure of different geographical regions, rates are calculated 
+        as if all regions shared the same age structure, that of the 2011 Canadian Census. Age-standardized rates 
+        are appropriate for comparing regions or trends over time. They are not a good representation of the 
+        burden of disease in the population.</li>
+        <li><b>Crude</b>:
+        These rates are not adjusted to the standard population, and represent the number of cases in a
+        specific geographic region divided by the population/population-at-risk in that region. 
+        Crude rates are representative of the burden of disease in the population.
+        </li>
+       </ul>
+       
+    "
